25a0dfbdf15029c58d21d92dc628a57d586d1312
Aamish Hussain <aamishhussain23@gmail.com>
2023-12-22 22:07:29 +0530

    changed cookie time

--- backend/utils/cookie.js
+++ backend/utils/cookie.js
@@ -6,7 +6,7 @@ const setCookie = (res, user, statusCode, message) => {
 
     res.status(statusCode).cookie('token', token, {
         httpOnly : true, 
-        maxAge : 30 * 60 * 1000, 
+        maxAge : 2 * 60 * 1000, 
         sameSite : process.env.NODE_ENV === "Development" ? "lax" : "none", 
         secure :   process.env.NODE_ENV === "Development" ? false : true
     }).json({
