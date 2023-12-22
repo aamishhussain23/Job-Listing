a07adb179b2bf40dcc1d3b4f2a9f804facb1fc9b
Aamish Hussain <aamishhussain23@gmail.com>
2023-12-22 22:22:49 +0530

    fixing error

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,9 +6,7 @@ const isAuthenticated = async (req, res, next) => {
     try {
         const {token} = req.cookies
         
-        if(!token){
-            return 
-        }
+        if(!token) return next(new ErrorHandler('Login First', 404))
         
         const data = jwt.verify(token, process.env.JWT_SECRET)
         req.user = await userCollection.findById(data._id)
--- backend/utils/cookie.js
+++ backend/utils/cookie.js
@@ -2,7 +2,6 @@ const jwt = require('jsonwebtoken')
 
 const setCookie = (res, user, statusCode, message) => {
     const token = jwt.sign({_id : user._id}, process.env.JWT_SECRET)
-    if(!token) return next(new ErrorHandler('Login First', 404))
 
     res.status(statusCode).cookie('token', token, {
         httpOnly : true, 
